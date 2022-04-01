45d9bcae2a4de07cff040f6165b855400a053c8e
Marcello Bonadias <marcelloebonadias@gmail.com>
2022-04-01 20:52:23 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int n;
+
+Console.WriteLine("Digite um número, por favor");
+n = Convert.ToInt32(Console.ReadLine());
+
+bool N2 = (n<0);
+
+if (N2)
+{
+    Console.WriteLine($"{n} é um número negativo");
+}
+else 
+{
+    Console.WriteLine();
+}
